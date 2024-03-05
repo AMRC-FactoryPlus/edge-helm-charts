dd88eee26f62fa1e7b046832baf824b1e0aa7886
Ben Morrow <b.morrow@amrc.co.uk>
2024-03-05 13:00:45 +0000

    Rework as a Helm chart
    We are now pre-rendering everything into the cluster git repo. The
    easiest way to do this is for everything to be a Helm chart.

--- flux-system/update.sh
+++ flux-system/update.sh
@@ -2,9 +2,13 @@
 
 set -x
 
-rm -f *.yaml
-
-flux install --export >flux-system.yaml
+flux install --export \
+    --cluster-domain xxx.cluster-domain.replace.invalid \
+    -n xxx-namespace-replace \
+    | sed -re'
+        s/xxx\.cluster-domain\.replace\.invalid/{{.Values.internalDomain}}/g;
+        s/xxx-namespace-replace/{{.Release.Namespace}}/g' \
+    >templates/flux-system.yaml
 
 git add .
 git commit -v
